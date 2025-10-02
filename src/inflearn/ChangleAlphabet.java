5b9333429c92b5a664c4e49c071d07115f082744
MyeongSooJang <mmsgod0427@gmail.com>
2025-10-02 23:35:21 +0900

    대소문자 바꾸기

---
+++ src/inflearn/ChangleAlphabet.java
@@ -0,0 +1,27 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class ChangleAlphabet {
+
+    public String solution(String word){
+        StringBuilder answer = new StringBuilder();
+        for(char c : word.toCharArray()){
+            if(Character.isLowerCase(c)){
+                answer.append(Character.toUpperCase(c));
+            } else {
+                answer.append(Character.toLowerCase(c));
+            }
+        }
+
+        return answer.toString();
+    }
+
+    public static void main(String[] args) {
+        ChangleAlphabet main = new ChangleAlphabet();
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        System.out.println(main.solution(input));
+        sc.close();
+    }
+}
