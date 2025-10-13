eb217b0990c2c3d73df4012cd57a5711ff1886db
MyeongSooJang <mmsgod0427@naver.com>
2025-10-13 22:21:44 +0900

    feat: 큰 수 출력하기

---
+++ src/inflearn/ReturnBigNumber.java
@@ -0,0 +1,25 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class ReturnBigNumber {
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        int input1 = in.nextInt();
+        in.nextLine();
+        String input2 = in.nextLine();
+
+        String[] input3 = input2.split(" ");
+        int[] arr = new int[input3.length];
+
+        for(int i =0; i<input3.length; i++){
+            arr[i] = Integer.parseInt(input3[i]);
+        }
+        System.out.print(arr[0] + " ");
+
+        for(int i =1; i<arr.length; i++){
+            if(arr[i]>arr[i-1]) System.out.print(arr[i] + " ");
+        }
+
+    }
+}
