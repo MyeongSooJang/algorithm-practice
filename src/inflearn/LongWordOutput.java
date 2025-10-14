bd922c1fb9b66a4884f68f6d4954d55105f78103
MyeongSooJang <mmsgod0427@gmail.com>
2025-10-14 23:14:06 +0900

    feat : 긴 문자열 출력하기

---
+++ src/inflearn/LongWordOutput.java
@@ -0,0 +1,22 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class LongWordOutput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String sentence = sc.nextLine();
+        String[] words = sentence.split(" ");
+
+        String longestWord = "";
+
+        for (String word : words) {
+            if (word.length() > longestWord.length()) {
+                longestWord = word;
+            }
+        }
+        System.out.println(longestWord);
+        sc.close();
+    }
+}
