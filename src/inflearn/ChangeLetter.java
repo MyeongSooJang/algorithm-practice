80a40a75eb7fef49cf3dd9693c815314dfbc743a
MyeongSooJang <mmsgod0427@naver.com>
2025-12-03 14:12:42 +0900

    feat: 특수문자를 제외하고 문자열 바꾸기
    - 양쪽에서의 시작점을 생성
    - 하나씩 증가/감소하면서 문자열인 경우에만 바꾸기

---
+++ src/inflearn/ChangeLetter.java
@@ -0,0 +1,32 @@
+package inflearn;
+
+import java.util.*;
+
+public class ChangeLetter {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+        String result = ChangeLetter.solution(input);
+        System.out.print(result);
+    }
+
+    public static String solution(String input) {
+        char[] chars = input.toCharArray();
+        int lt = 0;
+        int rt = chars.length - 1;
+        while(lt < rt) {
+            if (!Character.isLetter(chars[lt])) {
+                lt++;
+            } else if (!Character.isLetter(chars[rt])) {
+                rt--;
+            } else {
+                char temp = chars[lt];
+                chars[lt] = chars[rt];
+                chars[rt] = temp;
+                lt++;
+                rt--;
+            }
+        }
+        return String.valueOf(chars);
+    }
+}
