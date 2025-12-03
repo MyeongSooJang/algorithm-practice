eea4592d30450129fac54094bf9f22307f742ea5
MyeongSooJang <mmsgod0427@naver.com>
2025-12-03 15:12:08 +0900

    refactor: stream으로 더욱 개선

--- src/inflearn/basic/DeleteSame.java
+++ src/inflearn/basic/DeleteSame.java
@@ -6,14 +6,10 @@ import java.util.stream.*;
 
 public class DeleteSame {
     public static String solution(String input) {
-        char[] c = input.toCharArray();
-        List<Character> result = new ArrayList<>();
-        for (int i = 0; i < c.length; i++) {
-            if (!result.contains(c[i])) {
-                result.add(c[i]);
-            }
-        }
-        return result.stream().map(String::valueOf).collect(Collectors.joining());
+        return input.chars()
+                .distinct()
+                .mapToObj(c-> String.valueOf((char)c))
+                .collect(Collectors.joining());
     }
 
     public static void main(String[] args) {
