207ccb5e67062f7b54688f99c6777f0c9f646d7d
MyeongSooJang <mmsgod0427@naver.com>
2025-12-29 22:24:11 +0900

    feat: Bronze2 블랙잭 해결

---
+++ src/backjoon/bronze2/Problem2798.java
@@ -0,0 +1,38 @@
+package backjoon.bronze2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Problem2798 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        String cards = sc.nextLine();
+        solution(input, cards);
+    }
+
+    public static void solution(String input, String cards) {
+        int[] inputs = Arrays.stream(input.split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int max = inputs[1];
+
+        int[] card = Arrays.stream(cards.split(" "))
+                .mapToInt(Integer::parseInt)
+                .sorted().toArray();
+
+        int result = 0;
+
+        for (int i = 0; i < card.length; i++) {
+            for (int j = i + 1; j < card.length; j++) {
+                for (int k = j + 1; k < card.length; k++) {
+                    int sums = card[i] + card[j] + card[k];
+                    if (sums <= max && result < sums) {
+                        result = sums;
+                    }
+                }
+            }
+        }
+        System.out.println(result);
+    }
+}
