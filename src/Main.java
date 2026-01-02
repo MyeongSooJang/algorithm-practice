78d0e92e27233bcd14e8d4354e2cd94fb185e702
MyeongSooJang <mmsgod0427@naver.com>
2026-01-02 22:33:16 +0900

    feat: Bronze1 수 정렬하기3 해결

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,16 @@
 import java.util.*;
 
-class Main {
-    public static void main(String[] args) {
+class Main{
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        String cards = sc.nextLine();
-        solution(input, cards);
-    }
-
-    public static void solution(String input, String cards) {
-        int[] inputs = Arrays.stream(input.split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
-        int max = inputs[1];
-
-        int[] card = Arrays.stream(cards.split(" "))
-                .mapToInt(Integer::parseInt)
-                .sorted().toArray();
-
-        int result = 0;
-
-        for (int i = 0; i < card.length; i++) {
-            for (int j = i + 1; j < card.length; j++) {
-                for (int k = j + 1; k < card.length; k++) {
-                    int sums = card[i] + card[j] + card[k];
-                    if (sums <= max && result < sums) {
-                        result = sums;
-                    }
-                }
-            }
+        int count = sc.nextInt();
+        int[] inputs = new int[count];
+        for(int i = 0; i< count; i++){
+            inputs[i] = sc.nextInt();
         }
-        System.out.println(result);
+        solution(inputs);
+    }
+    public static void solution(int[] inputs){
+        Arrays.stream(inputs).sorted().forEach(System.out::println);
     }
 }
-
