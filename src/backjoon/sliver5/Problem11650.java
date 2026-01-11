b7542aa43916fd514b69f6d496db67291576f898
MyeongSooJang <mmsgod0427@naver.com>
2026-01-11 21:52:52 +0900

    refactor: Sliver5 좌표 정렬 이차원 배열로 변경

--- src/backjoon/sliver5/Problem11650.java
+++ src/backjoon/sliver5/Problem11650.java
@@ -3,24 +3,28 @@ package backjoon.sliver5;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class Problem11650 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        List<String> list = new ArrayList();
+        int[][] arr = new int[n][2];
         for (int i = 0; i < n; i++) {
-            list.add(br.readLine());
+            String[] str = br.readLine().split(" ");
+            arr[i][0] = Integer.parseInt(str[0]);
+            arr[i][1] = Integer.parseInt(str[1]);
         }
-        list.sort((x, y) -> {
-            if (Integer.parseInt(x.split(" ")[0]) != Integer.parseInt(y.split(" ")[0])) {
-                return Integer.parseInt(x.split(" ")[0]) - Integer.parseInt(y.split(" ")[0]);
-            } else {
-                return  Integer.parseInt(x.split(" ")[1]) -  Integer.parseInt(y.split(" ")[1]);
+        Arrays.sort(arr, (a, b) -> {
+            if (a[0] != b[0]) {
+                return a[0] - b[0];
             }
+            return a[1] - b[1];
         });
-        list.forEach(System.out::println);
+        StringBuilder sb = new StringBuilder();
+        for (int[] arrs : arr) {
+            sb.append(arrs[0]).append(" ").append(arrs[1]).append("\n");
+        }
+        System.out.println(sb);
     }
 }
