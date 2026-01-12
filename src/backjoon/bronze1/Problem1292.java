09cf21bcbc49ac23d281c4ee509a3d3e9dce9dee
MyeongSooJang <mmsgod0427@naver.com>
2026-01-12 15:25:21 +0900

    feat: Bronze1 쉽게 푸는 문제 문제 해결

--- src/backjoon/bronze1/Problem1292.java
+++ src/backjoon/bronze1/Problem1292.java
@@ -1,7 +1,11 @@
-import java.util.*;
-import java.io.*;
+package backjoon.bronze1;
 
-class Main{
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Problem1292 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] str = br.readLine().split(" ");
