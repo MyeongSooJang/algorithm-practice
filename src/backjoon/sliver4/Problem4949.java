429bcabbca63d193a61c600c51a88cdffdc52919
MyeongSooJang <mmsgod0427@naver.com>
2026-01-24 21:12:43 +0900

    feat: Sliver4 균형 잡힌 세상 문제

---
+++ src/backjoon/sliver4/Problem4949.java
@@ -0,0 +1,47 @@
+package backjoon.sliver4;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+
+public class Problem4949 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+                new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw =
+                new BufferedWriter(new OutputStreamWriter(System.out));
+        while (true) {
+            String s = br.readLine();
+            if (s.equals(".")) {
+                break;
+            }
+            bw.write(validateBalance(s) ? "yes\n" : "no\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+
+    public static boolean validateBalance(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[') {
+                stack.push(c);
+            } else if (c == ')') {
+                if (stack.isEmpty() || stack.pop() != '(') {
+                    return false;
+                }
+            } else if (c == ']') {
+                if (stack.isEmpty() || stack.pop() != '[') {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+
+    }
+}
