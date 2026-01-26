34d7aff21871c7224f9d1d1360e6eb8c181f5bfa
MyeongSooJang <mmsgod0427@naver.com>
2026-01-26 13:31:57 +0900

    feat: Sliver4 제로 문제 해결

--- src/backjoon/sliver4/Problem10773.java
+++ src/backjoon/sliver4/Problem10773.java
@@ -1,8 +1,14 @@
-import java.util.*;
-import java.io.*;
+package backjoon.sliver4;
 
-class Main{
-    public static void main(String[] args)throws IOException{
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+
+public class Problem10773 {
+    public static void main(String[] args)throws IOException {
         BufferedReader br =
                 new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw =
